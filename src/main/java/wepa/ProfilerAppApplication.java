6f6ef4fc0657bcd9631dd9bdad6dab7796de05d6
Mikko Tiainen <mikko.masala@gmail.com>
2016-12-10 16:44:30 +0200

    inital commit, hello world app

---
+++ src/main/java/wepa/ProfilerAppApplication.java
@@ -0,0 +1,11 @@
+package wepa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilerAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProfilerAppApplication.class, args);
+    }
+}
---
+++ src/main/java/wepa/controller/DefaultController.java
@@ -0,0 +1,15 @@
+package wepa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DefaultController {
+    
+    @RequestMapping("*")
+    @ResponseBody
+    public String hello() {
+        return "Hello World!";
+    }
+}
