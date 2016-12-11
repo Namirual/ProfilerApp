26a9da69412db853189db88342ae8087767338c6
jippo444 <toni.salama@helsinki.fi>
2016-12-11 13:50:00 +0200

    Tehty UserdetailsService, AccountRepository ja Account. Muokattu hieman DefaultSecurityConfia jotta saadaan tilit käyttöön myös testiversioon. Luotu DefaultUserInitService, joka luo kaksi käyttäjää testiversiossa.

---
+++ src/main/java/wepa/service/DefaultUserInitService.java
@@ -0,0 +1,33 @@
+package wepa.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import wepa.domain.Account;
+import wepa.repository.AccountRepository;
+
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
+
+@Service
+public class DefaultUserInitService {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private AccountRepository userDetailsRepository;
+    @PostConstruct
+    public void initDefaultUsers() {
+
+        Account user = new Account();
+        user.setUsername("reino");
+        user.setPassword(passwordEncoder.encode("huopatossu"));
+        user.setAuthorities(Arrays.asList("ADMIN"));
+        user = userDetailsRepository.save(user);
+
+        Account user2 = new Account();
+        user2.setUsername("maxwell");
+        user2.setPassword(passwordEncoder.encode("smart"));
+        user2.setAuthorities(Arrays.asList("USER"));
+        user2 = userDetailsRepository.save(user2);
+    }
+}
