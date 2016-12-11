63e2651947f79e96fe65cea7bed4257623ecf4e9
jippo444 <toni.salama@helsinki.fi>
2016-12-11 14:09:15 +0200

    Lisätty myös tuotantoon kaksi oletuskäyttäjää.

--- src/main/java/wepa/service/DefaultUserInitService.java
+++ src/main/java/wepa/service/DefaultUserInitService.java
@@ -1,6 +1,7 @@
 package wepa.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import wepa.domain.Account;
@@ -9,6 +10,7 @@ import wepa.repository.AccountRepository;
 import javax.annotation.PostConstruct;
 import java.util.Arrays;
 
+@Profile("default")
 @Service
 public class DefaultUserInitService {
     @Autowired
@@ -29,5 +31,6 @@ public class DefaultUserInitService {
         user2.setPassword(passwordEncoder.encode("smart"));
         user2.setAuthorities(Arrays.asList("USER"));
         user2 = userDetailsRepository.save(user2);
+        System.out.println(user2.getPassword());
     }
 }
