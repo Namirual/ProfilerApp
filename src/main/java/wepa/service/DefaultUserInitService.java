6de294eaac7df28c7063719e5f894da5990fa09a
jippo444 <toni.salama@helsinki.fi>
2016-12-11 14:39:31 +0200

    Lisätty myös tuotantoon kaksi oletuskäyttäjää, korjattu init-luokka.

--- src/main/java/wepa/service/DefaultUserInitService.java
+++ src/main/java/wepa/service/DefaultUserInitService.java
@@ -10,7 +10,7 @@ import wepa.repository.AccountRepository;
 import javax.annotation.PostConstruct;
 import java.util.Arrays;
 
-@Profile("default")
+
 @Service
 public class DefaultUserInitService {
     @Autowired
@@ -19,18 +19,20 @@ public class DefaultUserInitService {
     private AccountRepository userDetailsRepository;
     @PostConstruct
     public void initDefaultUsers() {
-
-        Account user = new Account();
-        user.setUsername("reino");
-        user.setPassword(passwordEncoder.encode("huopatossu"));
-        user.setAuthorities(Arrays.asList("ADMIN"));
-        user = userDetailsRepository.save(user);
-
-        Account user2 = new Account();
-        user2.setUsername("maxwell");
-        user2.setPassword(passwordEncoder.encode("smart"));
-        user2.setAuthorities(Arrays.asList("USER"));
-        user2 = userDetailsRepository.save(user2);
-        System.out.println(user2.getPassword());
+        if (userDetailsRepository.findByUsername("reino") == null) {
+            Account user = new Account();
+            user.setUsername("reino");
+            user.setPassword("$2a$10$3fT4VDT7t7N4bUtdajXa4ekC6NzAj9EO/08u920sR66wDFonlV.au");
+            user.setAuthorities(Arrays.asList("ADMIN"));
+            user = userDetailsRepository.save(user);
+        }
+        if (userDetailsRepository.findByUsername("maxwell") == null) {
+            Account user2 = new Account();
+            user2.setUsername("maxwell");
+            user2.setPassword("$2a$10$HOenmXvSwgDYdJLt5mvYoelgKWRem9UcFf.yTUOjal7aoOsvf2SWi");
+            user2.setAuthorities(Arrays.asList("USER"));
+            user2 = userDetailsRepository.save(user2);
+            System.out.println(user2.getPassword());
+        }
     }
 }
