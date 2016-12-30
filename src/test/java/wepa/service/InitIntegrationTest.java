4b4298097d75f1f167b804ca98d9b813ae93a255
jippo444 <toni.salama@helsinki.fi>
2016-12-30 16:46:25 +0200

    Korjattu initintegration -testi.

--- src/test/java/wepa/service/InitIntegrationTest.java
+++ src/test/java/wepa/service/InitIntegrationTest.java
@@ -125,12 +125,12 @@ public class InitIntegrationTest {
 
 
     }
-    
-    @Test
-    public void nonsenseTest() {
-        assertEquals(1,1);
-    }
-/*
+//
+//    @Test
+//    public void nonsenseTest() {
+//        assertEquals(1,1);
+//    }
+
     @Test
     public void canCreateProfileWithPic() {
 
@@ -173,5 +173,5 @@ public class InitIntegrationTest {
         assertNotEquals(answer2.getAnswer(), profile2.getProfileQuestions().get(1).getCorrectAnswer());
 
     }
-*/
+
 }
